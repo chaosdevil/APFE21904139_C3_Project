8d9c2faba5c4ed4eaaa3aabbd63d8ab779abd623
Liu Bei <surawankam@pop-os.localdomain>
2021-06-14 19:51:21 +0700

    Initial commit

---
+++ src/main/java/RestaurantService.java
@@ -0,0 +1,38 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
+        Restaurant foundRestaurant = null;
+        for (int i = 0; i < restaurants.size(); i++) {
+            if (restaurantName.compareTo(restaurants.get(i).getName()) == 0) {
+                foundRestaurant = restaurants.get(i);
+            }
+        }
+        if (foundRestaurant != null) {
+            return foundRestaurant;
+        }
+        throw new restaurantNotFoundException(restaurantName);
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+
+}
